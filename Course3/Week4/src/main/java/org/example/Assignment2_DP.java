9d8db3befd3b44a7eee217c2238c6a1126721b25
Thomas Borbeli <borbelithomas@gmail.com>
2023-08-19 16:39:47 +0300

    finish week1 assignment/course 4

--- Course3/Week4/src/main/java/org/example/Assignment2_DP.java
+++ Course3/Week4/src/main/java/org/example/Assignment2_DP.java
@@ -17,50 +17,64 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Assignment2_DP {
-    public static void main(String[] args) {
-        try {
-            String fileName = "Course3/Week4/src/main/resources/knapsack_big.txt";
+    public static void main(String[] args)
+    {
+        try{
+
+            String fileName = "Course3/Week4/src/main/resources/knapsack1.txt";
             List<Item> input = new ArrayList<>();
 
             try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+
                 String line = reader.readLine(); // Skip the first line
                 String[] split = line.split(" ");
-
-                int W = Integer.parseInt(split[0]);
-                int n = Integer.parseInt(split[1]);
+                Integer W = Integer.parseInt(split[0]);
+                Integer n = Integer.parseInt(split[1]);
 
                 while ((line = reader.readLine()) != null) {
                     String[] split2 = line.split(" ");
                     input.add(new Item(Integer.parseInt(split2[0]), Integer.parseInt(split2[1])));
+
                 }
 
-                HashMap<Integer, Long> dp = new HashMap<>();
-                dp.put(0, 0L);
+//                int[][] dp = new int[n][W + 1];
+//
+//                for(int i = 0; i <= W; i++) {
+//                    dp[0][i] = 0;
+//                }
 
-                for (int i = 1; i <= n; i++) {
-                    HashMap<Integer, Long> newDP = new HashMap<>(dp);
-                    for (int x : dp.keySet()) {
-                        if (x + input.get(i - 1).getWeight() <= W) {
-                            newDP.put(x + input.get(i - 1).getWeight(),
-                                    Math.max(newDP.getOrDefault(x + input.get(i - 1).getWeight(), 0L),
-                                            dp.get(x) + input.get(i - 1).getValue()));
+                HashMap<Integer, ArrayList<Integer>> mapWeightsToValuesForIItems = new HashMap<>();
+
+                for (int i = 1; i < n; i++) {
+                    for(int x = 0; x <= W; x++) {
+                        if (x - input.get(i).getWeight() < 0) {
+//                            dp[i][x] = dp[i - 1][x];
+                            ArrayList<Integer> iItems = mapWeightsToValuesForIItems.get(x);
+                            if (iItems == null) {
+                                ArrayList<Integer> newArr = new ArrayList<>();
+                                if(newArr.get(i-1) != null) {
+                                    newArr.set(i, iItems.get(i - 1));
+                                    mapWeightsToValuesForIItems.put(x, iItems);
+                                }
+                            }
+                            if(iItems.get(i-1) != null) {
+                                iItems.set(i, iItems.get(i - 1));
+                                mapWeightsToValuesForIItems.put(x, iItems);
+                            }
+
+                        }
+                        else {
+                            dp[i][x] = Math.max(dp[i - 1][x], dp[i - 1][x - input.get(i).getWeight()] + input.get(i).getValue());
                         }
                     }
-                    dp = newDP;
                 }
 
-                long max = 0;
-                for (long value : dp.values()) {
-                    max = Math.max(max, value);
-                }
-
-                System.out.println(max);
-
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
+                System.out.println(dp[n-1][W]);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
